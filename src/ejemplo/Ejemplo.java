1de80f0d3d50273a28fe31683b02e45a5b46217b
Marianowest <marian_west6@hotmail.com>
2023-08-09 16:20:32 -0300

    Initial commit

---
+++ src/ejemplo/Ejemplo.java
@@ -0,0 +1,15 @@
+
+ 
+package ejemplo;
+
+
+
+    /**
+     * @param args the command line arguments
+     */
+public static void main(String[] args) {
+//TODO code aplication logic here        
+ Persona maria = new Persona("Lopez","Maria",30);
+ maria.saludar();
+    
+
---
+++ src/ejemplo/Persona.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejemplo;
+
+/**
+ *
+ * @author User
+ */
+public class Persona {
+    private String apellido;
+    private String nombre;
+    private int edad;
+    public Persona (String apellido,String nombre,int edad){
+     this.apellido= apellido;
+     this.nombre= nombre;
+     this.edad=edad;
+     
+    }
+public Persona() {}
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+    
+    public void saludar (){ 
+       System.out.println("Hola mi nombre es "+ nombre);
+    }
+}
