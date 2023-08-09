ee65b2c61f3575836185955b65b04c072847645f
AmeliaGaddi <pecky991@gmail.com>
2023-08-09 16:45:18 -0300

    clase nueva
    agregue clase con mi nombre

---
+++ src/ejemplo/AmeliaG.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejemplo;
+
+public class AmeliaG {
+    
+public class Amelia{
+private int edad;
+    
+    public Amelia(int edad){
+        this.edad = edad;
+    }
+    
+    public void setEdad(int edad){
+        this.edad = edad;
+    }
+    public int getEdad(){
+        return this.edad;
+    }
+}
+
+    
+}
